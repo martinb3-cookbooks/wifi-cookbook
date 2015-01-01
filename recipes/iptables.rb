311706f41d9e8fd2dc580cad51899b37524ed5ee
Martin Smith <martin@mbs3.org>
2014-12-31 19:21:18 -0600

    Check array before adding

--- recipes/iptables.rb
+++ recipes/iptables.rb
@@ -4,20 +4,23 @@ node.set_unless['iptables']['rules'] = []
 ports = [80, 443, 8080, 8443, 8880, 8843, 3478]
 
 ports.each do |p|
-  node.set['iptables']['rules'] << {
+  rule = {
     name: "port_#{p}",
     value: "-A FWR -p tcp -m tcp --dport #{p} -j ACCEPT"
   }
+  node.set['iptables']['rules'] << rule if node['iptables']['rules'].select { |r| r[:name] == rule[:name] }.empty?
 end
 
-node.set['iptables']['rules'] << {
+port_10000 = {
   name: 'port_10000',
   value: '-A FWR -p tcp -m tcp --dport 10000:10010 -m state --state NEW -j ACCEPT'
 }
+node.set['iptables']['rules'] << port_10000 if node['iptables']['rules'].select { |r| r[:name] == port_10000[:name] }.empty?
 
-node.set['iptables']['rules'] << {
+port_10001 = {
   name: 'port_10001',
   value: '-A FWR -p tcp -m tcp --sport 10001 --dport 10001 -j ACCEPT'
 }
+node.set['iptables']['rules'] << port_10001 if node['iptables']['rules'].select { |r| r[:name] == port_10001[:name] }.empty?
 
 include_recipe 'martinb3-base::iptables'
